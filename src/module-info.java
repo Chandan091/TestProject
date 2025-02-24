eb33c341673e8cfdbfadcf54ca1867ba2a4c705e
chand <chand@CHANDAN>
2025-02-24 09:01:58 +0530

    Login Commit

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,7 @@
  * 
  */
 module TestProject {
+	requires org.seleniumhq.selenium.firefox_driver;
+	requires org.seleniumhq.selenium.chromium_driver;
+	requires org.seleniumhq.selenium.chrome_driver;
 }
